d3244ecc1bf0a5ccebebf14ab1823de0d224a942
ivoneijr <ivoneijr@gmail.com>
2019-12-21 19:07:28 -0300

    first functional flatten

--- src/routes/flatten.go
+++ src/routes/flatten.go
@@ -7,17 +7,14 @@ import (
 	"../helpers"
 )
 
-func test(matrix [][]string) <-chan string {
-	c := make(chan string)
+func makeFlat(matrix [][]string) <-chan string {
+	c := make(chan string, 9)
 
 	for _, row := range matrix {
 		for _, column := range row {
-			fmt.Println(column)
-			// c <- column
+			c <- column
 		}
 	}
-
-	fmt.Println("inserted")
 	close(c)
 
 	return c
@@ -26,20 +23,17 @@ func test(matrix [][]string) <-chan string {
 // Flatten route to matrix as a 1 line string, with values separated by commas.
 func Flatten(w http.ResponseWriter, r *http.Request) {
 	records, _ := helpers.GetRecords(w, r)
+	flatted := makeFlat(records)
 
-	test(records)
+	var response string
 
-	// for i := range flatted {
-	// 	fmt.Printf("%d ", i)
-	// }
+	for n := range flatted {
+		if len(flatted) == 0 {
+			response += fmt.Sprintf("%s", n)
+		} else {
+			response += fmt.Sprintf("%s,", n)
+		}
+	}
 
-	// var response string
-
-	// for _, row := range records {
-	// 	response = fmt.Sprintf("%s%s,", response, strings.Join(row, ","))
-	// }
-	// fmt.Fprint(w, response)
-
-	response := "flat response"
 	fmt.Fprint(w, response)
 }
