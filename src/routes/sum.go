3b478d05b667f9330a41a9bb283a46c051c452f1
ivoneijr <ivoneijr@gmail.com>
2019-12-29 17:24:32 -0300

    refactor Sum to use go routine

--- src/routes/sum.go
+++ src/routes/sum.go
@@ -1,20 +1,28 @@
 package routes
 
 import (
+	"../helpers"
 	"fmt"
 	"net/http"
-
-	"../helpers"
+	"sync"
 )
 
 // Sum Route to sum all values in the matrix
 func Sum(responseWriter http.ResponseWriter, request *http.Request) {
 	matrix, _ := helpers.GetMatrix(helpers.SUM, responseWriter, request)
-
 	count := 0
+	wg := sync.WaitGroup{}
+
 	for n := range matrix {
-		count += n
+		wg.Add(1)
+
+		go func(n int) {
+			defer wg.Done()
+			count += n
+		}(n)
 	}
+
+	wg.Wait()
 
 	fmt.Fprint(responseWriter, count)
 }
