20bc271dd4781c6e8132092b7541f5dab6bd8c7f
ivoneijr <ivoneijr@gmail.com>
2019-12-30 02:32:35 -0300

    refactor test suite

--- src/routes/handlers_test.go
+++ src/routes/handlers_test.go
@@ -2,85 +2,53 @@ package routes
 
 import "testing"
 
-func TestHealthCheckHandler(t *testing.T) {
-	response, err := getResponseFor(handlerParams{HealthCheckHandler, "/health-check", "GET"})
-	if err != nil {
-		t.Fatal(err)
+func TestAllHandlers(t *testing.T) {
+
+	suite := []struct {
+		params   handlerParams
+		expected string
+		message  string
+	}{
+		{
+			handlerParams{HealthCheckHandler, "/health-check", "GET"},
+			`{"alive": true}`,
+			"HealthCheckHandler returned unexpected content: got %v want %v",
+		},
+		{
+			handlerParams{Echo, "/echo", "POST"},
+			"1,2,3\n4,5,6\n7,8,9\n",
+			"Echo handler returned wrong content got: %v want %v",
+		},
+		{
+			handlerParams{Flatten, "/flatten", "POST"},
+			"1,2,3,4,5,6,7,8,9",
+			"Flatten handler returned wrong content got: %v want %v",
+		},
+		{
+			handlerParams{Invert, "/invert", "POST"},
+			"1,4,7\n2,5,8\n3,6,9\n",
+			"Invert handler returned wrong content got: %v want %v",
+		},
+		{
+			handlerParams{Multiply, "/multiply", "POST"},
+			"362880",
+			"Multiply handler returned wrong content got: %v want %v",
+		},
+		{
+			handlerParams{Sum, "/sum", "POST"},
+			"45",
+			"Sum handler returned wrong content got: %v want %v",
+		},
 	}
 
-	expected := `{"alive": true}`
-	if response != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			response, expected)
-	}
-}
+	for _, test := range suite {
+		response, err := getResponseFor(test.params)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func TestEcho(t *testing.T) {
-	response, err := getResponseFor(handlerParams{Echo, "/echo", "POST"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "1,2,3\n4,5,6\n7,8,9\n"
-
-	if response != expected {
-		t.Errorf("Echo handler returned wrong content got %v want %v",
-			response, expected)
-	}
-}
-
-func TestFlatten(t *testing.T) {
-	response, err := getResponseFor(handlerParams{Flatten, "/flatten", "POST"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "1,2,3,4,5,6,7,8,9"
-
-	if response != expected {
-		t.Errorf("Flatten handler returned wrong content got %v want %v",
-			response, expected)
-	}
-}
-
-func TestInvert(t *testing.T) {
-	response, err := getResponseFor(handlerParams{Invert, "/invert", "POST"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "1,4,7\n2,5,8\n3,6,9\n"
-
-	if response != expected {
-		t.Errorf("Invert handler returned wrong content got %v want %v",
-			response, expected)
-	}
-}
-
-func TestMultiply(t *testing.T) {
-	response, err := getResponseFor(handlerParams{Multiply, "/multiply", "POST"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "362880"
-
-	if response != expected {
-		t.Errorf("Multiply handler returned wrong content got %v want %v",
-			response, expected)
-	}
-}
-
-func TestSum(t *testing.T) {
-	response, err := getResponseFor(handlerParams{Sum, "/sum", "POST"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "45"
-
-	if response != expected {
-		t.Errorf("Multiply handler returned wrong content got %v want %v",
-			response, expected)
+		if response != test.expected {
+			t.Errorf(test.message, response, test.expected)
+		}
 	}
 }
